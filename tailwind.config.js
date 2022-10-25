9280d4f407c2e3fe4896090a498423b390167128
Steve <sntakirutimana72@gmail.com>
2022-10-25 09:09:44 +0200

    Adjust tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+const TailwindcssFormsPlugins = require('@tailwindcss/forms');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
@@ -12,6 +14,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    TailwindcssFormsPlugins,
   ],
-}
+};
