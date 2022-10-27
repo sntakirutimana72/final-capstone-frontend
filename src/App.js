6d383735fc1e7b8c2db93c13edb6ff7f9cb1e389
aliatayee <aliaqaatayee@gmail.com>
2022-10-27 11:49:59 +0430

    Update sidebar to make responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SideNav from './Components/SideNav/SideNav';
-import Rooms from './Components/Rooms/Rooms';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <main className="flex">
+        <main className="">
           <SideNav />
           <Routes>
-            <Route exact path="/" element={<Rooms />} />
-            <Route exact path="/rooms" element={<Rooms />} />
+            <Route exact path="/" />
+            <Route exact path="/rooms" />
             <Route exact path="/reserve" />
             <Route exact path="/my-reservations" />
             <Route exact path="/add-room" />
--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,76 +1,134 @@
+import * as React from 'react';
 import { NavLink } from 'react-router-dom';
-import {
-    FaFacebook,
-    FaPinterest,
-    FaTwitter,
-    FaInstagram,
-    FaLinkedinIn
-} from "react-icons/fa";
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
 
-const SideNav = () => {
-    const links = [
-        {
-            id: 1,
-            path: '/rooms',
-            text: 'ROOMS',
-        },
-        {
-            id: 2,
-            path: '/reserve',
-            text: 'Reserve Form',
-        },
-        {
-            id: 3,
-            path: '/my-reservations',
-            text: 'My Reservations',
-        },
-        {
-            id: 4,
-            path: '/add-room',
-            text: 'Add Room',
-        },
+const drawerWidth = 240;
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
-        {
-            id: 5,
-            path: '/delete-room',
-            text: 'Delete Room',
-        },
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
-    ];
-    return (
+export default function SideNav() {
+  const links = [
+    {
+      id: 1,
+      path: '/rooms',
+      text: 'ROOMS',
+    },
+    {
+      id: 2,
+      path: '/reserve',
+      text: 'Reserve Form',
+    },
+    {
+      id: 3,
+      path: '/my-reservations',
+      text: 'My Reservations',
+    },
+    {
+      id: 4,
+      path: '/add-room',
+      text: 'Add Room',
+    },
 
-        <div className="flex flex-col h-screen  bg-white shadow w-60 justify-between">
-            <div className="space-y-3">
-                <div className="flex h-44 justify-center pt-5">
-                    <h2 className="text-xl font-bold">EASY BOOKING</h2>
-                </div>
-                <div className="flex-1 pl-5">
-                    <ul className="pt-2 pb-4 space-y-1 text-sm">
-                        {links.map((link) => (
-                            <li className="">
+    {
+      id: 5,
+      path: '/delete-room',
+      text: 'Delete Room',
+    },
 
-                                <NavLink to={link.path}  key={link.id} className={({ isActive }) => (isActive ? 'active flex h-16 font-black text-xl items-center pl-4 space-x-3 bg-lime-500 text-white' : ' h-16 font-black text-xl flex items-center pl-4 space-x-3')}>
-                                    {link.text}
-                                </NavLink>
+  ];
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-                            </li>
-                        ))
-                        }
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <div className='flex justify-center'>
-                    <a href="facebook.com" className='p-3'> <FaFacebook /></a>
-                    <a href="instagram.com" className='p-3'><FaInstagram /></a>
-                    <a href="linkedin.com" className='p-3'><FaLinkedinIn /></a>
-                    <a href="twitter.com" className='p-3'><FaTwitter /></a>
-                    <a href="pinterest.com" className='p-3'><FaPinterest /></a>
-                </div>
-                &copy; 2022 easybooking.
-            </div>
-        </div>
-    );
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Easy Booking
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {links.map((link) => (
+            <ListItem button component={NavLink} to={link.path} key={link.path} className="h-16 font-bold text-lg ">
+              {link.text}
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
 }
-
-export default SideNav;
