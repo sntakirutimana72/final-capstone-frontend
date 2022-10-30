ebe70f6b1c48ca67480177d60cb71e7d10bcc496
Chris Siku <chrissiku5@gmail.com>
2022-10-30 12:39:15 +0200

    Add Single Room component boilerplate

--- src/Pages/Room/Room.js
+++ src/Pages/Room/Room.js
@@ -1,7 +1,17 @@
-const Room = () => (
-  <div>
-    Room
-  </div>
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Room = ({
+  id, name, price, description,
+}) => (
+  <NavLink key={id} to={`/room/${id}`}>
+    <div>
+      <p>{name}</p>
+      <p>{price}</p>
+      <p>{description}</p>
+    </div>
+  </NavLink>
 );
 
 export default Room;
