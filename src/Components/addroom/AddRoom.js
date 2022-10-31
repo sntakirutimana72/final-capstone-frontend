27bc09fa26d7c3325bc8c610020e92a78c4974e7
safar ali <miqra019@gmail.com>
2022-10-31 16:37:59 +0500

    Solve linters

--- src/Components/addroom/AddRoom.js
+++ src/Components/addroom/AddRoom.js
@@ -24,59 +24,59 @@ export default function AddRoom() {
   };
 
   return (
-    
-      <div className={classes.add_book_dev}>
-        <form onSubmit={handleSubmit}>
-          <div className={classes.form}>
-            <label htmlFor="picture">Picture</label>
-            <input
-              type="text"
-              name="Picture"
-              value={state.picture}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className={classes.form}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              value={state.name}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className={classes.form}>
-            <label htmlFor="number_of_beds">Number of Beds</label>
-            <input
-              type="text"
-              name="number of beds"
-              value={state.number_of_beds}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className={classes.form}>
-            <label htmlFor="price">Price</label>
-            <input
-              type="text"
-              name="price"
-              value={state.price}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className={classes.form}>
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              name="description"
-              value={state.description}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className={classes.button}>
-            <button type="submit">Create Room</button>
-          </div>
-        </form>
-      </div>
-   
+
+    <div className={classes.add_book_dev}>
+      <form onSubmit={handleSubmit}>
+        <div className={classes.form}>
+          <label htmlFor="picture">Picture</label>
+          <input
+            type="text"
+            name="Picture"
+            value={state.picture}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={classes.form}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={classes.form}>
+          <label htmlFor="number_of_beds">Number of Beds</label>
+          <input
+            type="text"
+            name="number of beds"
+            value={state.number_of_beds}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={classes.form}>
+          <label htmlFor="price">Price</label>
+          <input
+            type="text"
+            name="price"
+            value={state.price}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={classes.form}>
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            value={state.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={classes.button}>
+          <button type="submit">Create Room</button>
+        </div>
+      </form>
+    </div>
+
   );
 }
