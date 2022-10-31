916f23a450945a519726d391c6934f3026536dd0
Chris Siku <chrissiku5@gmail.com>
2022-10-31 09:58:48 +0200

    Style the single room component

--- src/Pages/Room/Room.js
+++ src/Pages/Room/Room.js
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './style.css';
 
 const Room = ({
-  id, name, price, description,
+  id,
+  name,
+  picture,
+  price,
 }) => (
-  <NavLink key={id} to={`/room/${id}`}>
-    <div>
-      <p>{name}</p>
-      <p>{price}</p>
-      <p>{description}</p>
+  <NavLink key={id} to={`/room/${id}`} className="link">
+    <div className="room-card">
+      <div className="room-image-container">
+        <img src={picture} alt={name} className="room-image" />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h3>{name}</h3>
+        <h5 className="room-price">
+          $
+          {' '}
+          {price}
+        </h5>
+      </div>
     </div>
   </NavLink>
 );
--- src/Pages/Room/Rooms.js
+++ src/Pages/Room/Rooms.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Room from './Room';
 
-const Rooms = () => (
-  <>
-    <h1>Here is the list of all room</h1>
-    <Room />
-  </>
-);
+const Rooms = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/rooms').then((response) => {
+      //   console.log(response);
+      setRooms(response.data.rooms);
+    });
+  }, []);
+
+  return (
+    <>
+      {rooms.map((room) => (
+        <Room
+          key={room.id}
+          picture={room.picture}
+          name={room.name}
+          price={room.price}
+        />
+      ))}
+    </>
+  );
+};
 
 export default Rooms;
