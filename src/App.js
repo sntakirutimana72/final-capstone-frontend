6a676a3931cf5f2ae6b3744189d90673c7415227
Steve <sntakirutimana72@gmail.com>
2022-11-01 10:41:59 +0200

    Adjust app entry points

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { SignedInRoute } from './Components/middlewares';
+import { SignedInRoute, UserRequiredRoute } from './components/middlewares';
 import {
-  Signin, Register, PasswordReset, NewPassword,
-} from './Components/authentication';
-import SideNav from './Components/SideNav/SideNav';
-import Room from './Pages/Room/Room';
+  Register,
+  Signin,
+  PasswordReset,
+  NewPassword,
+} from './components/pages/authentication';
+import MyReserves from './components/pages/reservation';
+import SideNav from './components/navigation/SideNav';
+import SessionCtxProvider from './contexts/session';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <main className="flex h-screen">
-          <SideNav />
-          <Routes>
-            <Route element={<SignedInRoute />}>
-              <Route path="login" element={<Signin />} />
-              <Route path="register" element={<Register />} />
-              <Route path="set-new-password" element={<NewPassword />} />
-              <Route path="reset-password" element={<PasswordReset />} />
-            </Route>
-            <Route exact path="/" />
-            <Route exact path="/rooms" element={<Room />} />
-            <Route exact path="/reserve" />
-            <Route exact path="/my-reservations" />
-            <Route exact path="/add-room" />
-            <Route exact path="/delete-room" />
-          </Routes>
-        </main>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <SessionCtxProvider>
+    <Router>
+      <SideNav />
+      <section className="w-full">
+        <Routes>
+          <Route element={<SignedInRoute />}>
+            <Route path="login" element={<Signin />} />
+            <Route path="register" element={<Register />} />
+            <Route path="set-new-password" element={<NewPassword />} />
+            <Route path="reset-password" element={<PasswordReset />} />
+          </Route>
+
+          <Route element={<UserRequiredRoute />}>
+            <Route exact path="/reservations/mine" element={<MyReserves />} />
+          </Route>
+
+          <Route exact path="/my-reservations" element={<MyReserves />} />
+          {/* <Route exact path="/" />
+          <Route exact path="/rooms" />
+          <Route exact path="/reserve" />
+
+          <Route exact path="/add-room" />
+          <Route exact path="/delete-room" /> */}
+        </Routes>
+      </section>
+    </Router>
+  </SessionCtxProvider>
+);
 
 export default App;
