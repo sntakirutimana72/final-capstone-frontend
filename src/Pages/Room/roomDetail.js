42084cfd2f18337816c532e38cd2d0ca322db2c9
Olawale <olapetanwale@outlook.com>
2022-11-01 13:51:57 +0100

    Add icons and fix linter errors

--- src/Pages/Room/roomDetail.js
+++ src/Pages/Room/roomDetail.js
@@ -2,23 +2,36 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './RoomDetail.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarCheck } from '@fortawesome/free-regular-svg-icons';
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const RoomDetail = () => {
   const [roomDetail, setRoomDetail] = useState([]);
+  // const [message, setMessage] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     axios.get(`http://localhost:3000/api/v1/rooms/${id}`)
       .then((response) => {
         setRoomDetail(response.data);
-        console.log(response.data);
       });
   }, []);
 
-  // const capitalize = (string) => {
-  //   const string1 = string.replace(/^\s+|\s+$/gm,'');
-  //   return string1.charAt(0).toUpperCase() + string1.slice(1);
-  // };
+  const reserve = () => {
+    useEffect(() => {
+      axios.post(`http://localhost:3000/api/v1/rooms/${id}/reserve`, {
+        body: JSON.stringify({
+          room_id: roomDetail.id,
+          user_id: roomDetail.user_id,
+        }),
+        // .then((response) => {
+        //   setMessage(response.data);
+        // }),
+      });
+    }, []);
+  };
+
   return (
     <>
       <div className="room-img">
@@ -30,19 +43,42 @@ const RoomDetail = () => {
           *
           {roomDetail.description}
         </h1>
-        <ul>
+        <ul className="left-panel">
           <li>
-            <span className="room-info-title">Capacity: 2 Persons</span>
+            <span className="room-info-title">
+              Capacity
+            </span>
+            <span>
+              {roomDetail.number_of_beds}
+              {' '}
+              Persons
+            </span>
           </li>
           <li>
             <span>
-              Price per night:
+              Price per night
+            </span>
+            <span>
+              $
               {roomDetail.price}
             </span>
           </li>
+          <li>
+            <span>
+              Reservation fee
+            </span>
+            <span>
+              $
+              {(roomDetail.price / 3).toFixed(2)}
+            </span>
+          </li>
         </ul>
-        <button className="reserve-btn" type="submit">
-          Reserve
+        <button className="reserve-btn" type="submit" onClick={reserve}>
+          <FontAwesomeIcon className="font-calendar" icon={faCalendarCheck} />
+          <span>
+            Reserve
+          </span>
+          <FontAwesomeIcon className="font-arrow" icon={faCircleChevronRight} />
         </button>
       </div>
     </>
