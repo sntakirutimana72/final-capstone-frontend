0e1ce403a449d6e6835f968328d45108c5dabd5d
aliatayee <aliaqaatayee@gmail.com>
2022-11-02 16:21:27 +0430

    Add rooms select list

--- src/components/pages/add-reservation/ReserveForm.js
+++ src/components/pages/add-reservation/ReserveForm.js
@@ -1,13 +1,18 @@
 /* eslint-disable */
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useDispatch,  useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addNewReserve } from '../../../redux/features/reservations/mine';
-import { fetchRoomList } from '../../../redux/features/reservations/mine';
+import { getRooms } from '../../../redux/roomsSlice';
 
 function ReserveForm() {
     const dispatch = useDispatch();
+    const { data, loading, error } = useSelector((state) => state.rooms);
+
+    useEffect(() => {
+        dispatch(getRooms());
+    }, [dispatch]);
 
     const [from_date, setFrom_date] = useState('');
     const [to_date, setTo_date] = useState('');
@@ -27,9 +32,8 @@ function ReserveForm() {
             room_id,
         };
         dispatch(addNewReserve(obj));
-        navigate('/my-reservations')
+        navigate('/rooms')
     }
-
     return (
         <>
             <div className="flex w-full justify-center pt-10">
@@ -62,9 +66,9 @@ function ReserveForm() {
                                 <div className="relative">
                                     <select onChange={onRoomChanged} className="block appearance-none w-full border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                         <option defaultValue={0}>Please select</option>
-                                        <option value={1}>Room 1</option>
-                                        <option value={2}>Room 2</option>
-                                        <option value={3}>Room 3</option>
+                                        {data.map((room) => (
+                                            <option value={room.id} key={room.id}>{room.name}</option>
+                                        ))}
                                     </select>
                                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
