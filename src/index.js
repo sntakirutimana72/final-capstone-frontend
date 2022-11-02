bc5a578c6ef1b781d9136f7f22691cd51f9062b3
aliatayee <aliaqaatayee@gmail.com>
2022-11-02 10:30:28 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
