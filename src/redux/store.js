eeb2553ea2143e43393f64d4468fc4d69b8d672a
safar ali <miqra019@gmail.com>
2022-11-02 14:18:11 +0500

    Configure store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-import { createSlice } from '@reduxjs/toolkit';
 
 
 const initialState = [];
@@ -36,48 +35,4 @@ const roomsReducer = (state = {}, action = {}) => {
 
 export default roomsReducer;
 
-/*
-export const roomInfo = createSlice({
-  name: 'rooms',
-  initialState,
-  reducers: {
-    rooms(state, action) {
-      const details = action.payload.map((room) => ({
-        name: room.name,
-        number_of_beds: room.number_of_beds,
-        picture: room.picture,
-        description: room.description,
-        price: room.price,
-        user_id: room.user_id,
-        room_type_id: room.room_type_id,
-      }));
-      return details;
-    },
 
-  },
-});
-
-export const pageActions = roomInfo.actions;
-
-const addRoomEndpoint = 'http://localhost:3000/rooms';
-
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    name: name,
-    number_of_beds: number_of_beds,
-    picture: picture,
-    price: price,
-    description: description,
-    user_id: user_id,
-    room_type_id: room_type_id,
-  }),
-};
-
-const response = await fetch(addRoomEndpoint, options);
-const jsonResponse = await response.json();
-// console.log(jsonResponse);
-*/
