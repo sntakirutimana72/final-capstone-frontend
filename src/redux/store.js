43051be212e035a12c0417a9da93c13d7b70a6b6
safar ali <miqra019@gmail.com>
2022-11-02 14:20:18 +0500

    Complete posting data to api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 
 
 const initialState = [];
-const BASE_URL = 'http://127.0.0.1:3001/'
+const BASE_URL = 'http://127.0.0.1:3000/'
 const CREATE = 'room-app/room/CREATE';
 
 export const addRoom = (room) => ({
